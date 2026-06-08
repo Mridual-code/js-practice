da982f3312c2fba5c89fc194050dc806e2fc264f
Mridual-code <mridual.14910@stu.upes.ac.in>
2026-06-08 20:47:53 +0530

    completed task

--- function.js
+++ function.js
@@ -1,42 +1,54 @@
-//function declaration 1:
-function myself(){
-    console.lof("my name is Mridul");
+// Function Declaration 1
+function myself() {
+    console.log("My name is Mridul");
 }
 myself();
- //function declaration 2:
-function add(a,b){
-    return a+b;
+
+// Function Declaration 2
+function add(a, b) {
+    return a + b;
 }
-console.log(add(5,10));
-//function expression 1:
-const greetings = function(name){
+console.log(add(5, 10));
+
+// Function Expression 1
+const greetings = function(name) {
     console.log("Hello " + name);
-}
+};
 greetings("Mridul");
-//function expression 2:
-const multiply= function(x,y){
-    console.log(x*y),
-}
-multiply(5,10);
-// arrow function 1:
+
+// Function Expression 2
+const multiply = function(x, y) {
+    console.log(x * y);
+};
+multiply(5, 10);
+
+// Arrow Function 1
 const subtract = (a, b) => {
-  return a - b;
+    return a - b;
 };
 console.log(subtract(10, 3));
-// arrow function 2:
+
+// Arrow Function 2
 const divide = (x, y) => x / y;
 console.log(divide(20, 5));
-//callback function 1:
+
+// Callback Function 1
 function greet(name, callback) {
     console.log("Hello " + name);
+    callback();
 }
-functionbye(){
+
+function bye() {
     console.log("Goodbye!");
-}greet("Mridul", bye);
-//callback function 2:
+}
+
+greet("Mridul", bye);
+
+// Callback Function 2
 function processUser(callback) {
     callback();
 }
+
 processUser(() => {
     console.log("User processed");
 });
