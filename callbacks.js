965d4ca400f7022ab8e041bde563c9ce87ee4415
Mridual-code <mridual.14910@stu.upes.ac.in>
2026-06-09 15:30:26 +0530

    completed promise task

--- callbacks.js
+++ callbacks.js
@@ -25,3 +25,41 @@ function showMessage() {
   console.log("This message is delayed!");
 }
 delayedMessage(showMessage);
+//example of promise resolve:
+let promiseResolve = new Promise((resolve, reject) => {
+  resolve("Task completed successfully!");
+});
+promiseResolve.then((message) => {
+  console.log(message);
+});
+
+
+//example of promise reject:
+let promiseReject = new Promise((resolve, reject) => {
+  reject("Something went wrong!");
+});
+promiseReject
+  .then((message) => {
+    console.log(message);
+  })
+  .catch((error) => {
+    console.log("Error: " + error);
+  });
+
+
+  //example of chaninig multiple.then() methods:
+  let promiseChain = new Promise((resolve, reject) => {
+  resolve(5);
+});
+promiseChain
+  .then((num) => {
+    console.log("First then: " + num);
+    return num * 2;
+  })
+  .then((num) => {
+    console.log("Second then: " + num);
+    return num + 3;
+  })
+  .then((num) => {
+    console.log("Third then: " + num);
+  });
